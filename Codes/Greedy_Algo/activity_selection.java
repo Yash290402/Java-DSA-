3224f53245dc15a3cb9400b34b87f997bdb37e9b
Yash290402 <yash56774@gmail.com>
2024-05-08 11:44:16 +0530

    add

--- Codes/Greedy_Algo/activity_selection.java
+++ Codes/Greedy_Algo/activity_selection.java
@@ -1,8 +1,4 @@
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 public class activity_selection {
     public static void main(String[] args) {
